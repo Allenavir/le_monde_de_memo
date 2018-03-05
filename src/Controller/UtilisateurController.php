726e1a5aee9e1c934f277e8c13b87ebaf160a64f
Nico <hunter59282@live.fr>
2018-03-05 15:23:42 +0100

    avec css

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -29,5 +29,101 @@ class UtilisateurController extends Controller
             array( "utilisateur" => $bdd )
         );      
     }
+
+     /**
+    *@Route("utilisateur", name="DetailsUtilisateur")
+     */
+    public function displayMemo(){          
+               
+        return $this->render(
+            "detailsUtilisateur.html.twig")     
+        ;      
+       }
+
+
+    /**
+    * @Route("utilisateur/inscription", name="AddUtilisateur")
+    */
+        
+    public function AddMemo(Request $request){        
+        $utilisateur = new Utilisateur();                    
+        $form= $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+       
+        $msg="";
+
+        if($form->isSubmitted() ){
+
+        if($form->isValid()){
+            
+            $manager = $this->getDoctrine()->getManager();
+            $memo = $form->getData();           
+            $manager->persist($utilisateur);
+            $manager->flush();
+            
+            
+
+            $msg= "Memo ajouté avec succes";           
+            }
+
+        else{
+            $msg= "Vous avez oublié de remplir un champs";
+              }
+          }
+        
+        return $this->render("FormUtilisateur.html.twig", array("form"=>$form->createView(),"msg"=>$msg)); 
+        }           
+        
+   
     
-}    
+     /**
+    *@Route("utilisateur/supprimer/{id}", name="SuppUtilisateur",requirements={"id"="\d*"})
+     */
+    public function SuppMemo(Utilisateur $utilisateur){
+        $repo = $this->getDoctrine()->getManager();
+        $repo->remove($utilisateur);
+        $repo->flush();
+
+        return $this->render("listeUtilisateurs.html.twig", array("utilisateur"=>$utilisateur));    
+    } 
+
+
+    
+     /**
+    *@Route("utilisateur/modifier/{id}", name="modifUtilisateur",requirements={"id"="\d*"})
+     */
+    public function modifMemo(Request $request, Utilisateur $Utilisateur){
+
+        $user = $this->getUser();
+        $user->getId();
+        $user->getUsername();
+        $user->getMail(); 
+
+        $form= $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+        
+        $msg="";
+
+        if($form->isSubmitted() ){
+
+        if($form->isValid()){
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            $msg= "Produit modifié avec succes";   
+            return $this->render("detailsUtilisateur.html.twig", array( "utilisateur"=>$utilisateur )
+            );       
+
+            }
+
+        else{
+            $msg= "Vous avez oublié de remplir un champs";
+            }
+        }
+
+        return $this->render("FormUtilisateur.html.twig", array("form"=>$form->createView(),"msg"=>$msg));    
+    } 
+}
+    
